d9b7be992fa23c3908f8a3098609f88da31e378d
gargi806 <70997731+gargi806@users.noreply.github.com>
2023-02-03 12:18:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1",{},"hello");
+
+const heading2 = React.createElement("h2",{},"everyone");
+
+const heading3 = React.createElement("h2",{},[heading,heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("first"));
+root.render(heading3);
